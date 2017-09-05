f27b56895812dfa5dd7cfa7c82bb05ce6a404518
Nicholas Fallon <nickfallonwastaken@gmail.com>
2017-09-04 18:25:34 -0600

    circles of increasing size

--- bar-chart/part-2/app.js
+++ bar-chart/part-2/app.js
@@ -1,39 +1,43 @@
-var width = 420,
-    barHeight = 20
+var width = 960,
+    height = 500
 
 var x = d3.scaleLinear()
-    .range([0, width])
+    .range([height, 0])
 
 var chart = d3.select(".chart")
     .attr("width", width)
+    .attr("height", height);
 
 d3.tsv("data.tsv", type, function(error, data) {
-  x.domain([0, d3.max(data, function(d) {
+  y.domain([0, d3.max(data, function(d) {
     return d.value;
   })]);
 
-  chart.attr("height", barHeight * data.length)
+  var barWidth = width / data.length;
 
 
   var bar = chart.selectAll("g")
       .data(data)
     .enter().append("g")
       .attr("transform", function(d, i) {
-        return "translate(0," + i * barHeight + ")";
+        return "translate(" + i * barWidth + ", 0)";
       });
 
   bar.append("rect")
-      .attr("width", function(d) {
-        return x(d.value)
+      .attr("y", function(d) {
+        return y(d.value)
       })
-      .attr("height", barHeight - 1);
+      .attr("height", function(d) {
+        return height - y(d.value)
+      })
+      .attr("width", barWidth - 1);
 
 bar.append("text")
-    .attr("x", function(d) {
-      return x(d.value) - 3;
+    .attr("x", barWidth / 2)
+    .attr("y", function(d) {
+      return y(d.value) + 3;
     })
-    .attr("y", barHeight / 2)
-    .attr("dy", ".35em")
+    .attr("dy", ".75em")
     .text(function(d) {
       return d.value;
     });
